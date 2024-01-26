f21b79bbf57d3e6f3d2b9ce9f53ba1bba6f13585
pranayjain099 <pranayjain2001@gmail.com>
2024-01-27 01:00:16 +0530

    Creating table and adding dummy data on activation of plugin

--- wp-content/plugins/myplugin/myplugin.php
+++ wp-content/plugins/myplugin/myplugin.php
@@ -16,6 +16,30 @@ if (!defined("ABSPATH")) {
 // function of activation hook
 function my_plugin_activation()
 {
+    // to communicate with database we use variable "wpdb".
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix . 'emp';
+
+    // now generating query
+    $q = "CREATE TABLE IF NOT EXISTS`$wp_emp` (`ID` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL , `status` BOOLEAN NOT NULL , PRIMARY KEY (`ID`)) ENGINE = InnoDB;";
+
+    // now executing it.
+    $wpdb->query($q);
+
+    // if you want to insert the data two way
+
+    // first way
+    // $q1="INSERT INTO `$wp_emp` ( `name`, `email`, `status`) VALUES ('Pranay Jain', 'pranayjain2001@gmail.com', 1);";
+
+    // second way
+    $data = array(
+        'name' => 'Astha',
+        'email' => 'pranayjain2001@gmail.com',
+        'status' => 1
+    );
+
+    // executing it
+    $wpdb->insert($wp_emp, $data);
 
 }
 
@@ -26,8 +50,10 @@ register_activation_hook(__FILE__, 'my_plugin_activation');
 function my_plugin_deactivation()
 {
 
+
 }
 register_deactivation_hook(__FILE__, 'my_plugin_deactivation');
+
 
 
 ?>
